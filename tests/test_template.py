7aba34e58518d48e05b5b9796ed8e895c530961b
teprust <58107258+teprust@users.noreply.github.com>
2025-03-10 20:46:26 +0300

    Update test_template.py

--- tests/test_template.py
+++ tests/test_template.py
@@ -4,11 +4,11 @@ from src.calc import calculate
 def test_calc_division(): # тестирование деления
     assert calculate(10, 2, '/') == 5
 
-# def test_calc_subtraction(): # тестирование вычитания
-#     assert ...[добавьте ваш код здесь]
+def test_calc_subtraction(): # тестирование вычитания
+    assert calculate(10, 2, '-') == 8
 #
-# def test_calc_multiplication(): # тестирование умножение
-#     assert ...[добавьте ваш код здесь]
+def test_calc_multiplication(): # тестирование умножение
+    assert calculate(10, 2, '*') == 20
 #
-# def test_calc_addition(): # тестирование cложение
-#     assert ...[добавьте ваш код здесь]
+def test_calc_addition(): # тестирование cложения
+    assert calculate(10, 2, '+') == 12
